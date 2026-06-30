778cbfc628ddeeeb030afe229eb24858e0f28448
Tomas Bozzano <tomasbozzano@arjun.gob.ar>
2026-06-30 17:30:14 -0300

    add component X

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -1,6 +1,7 @@
 const nav = document.getElementById("header-nav");
 const links = document.querySelectorAll(".nav-link");
 const mobileLinks = document.getElementById("esquina");
+const logoLink = document.getElementById("logo-link");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
@@ -10,6 +11,10 @@ window.addEventListener("scroll", () => {
     }
 });
 
+
+logoLink.addEventListener("click", () => {
+    links.forEach(l => l.classList.remove("active"));
+});
 
 links.forEach(link => {
     link.addEventListener("click", () => {
