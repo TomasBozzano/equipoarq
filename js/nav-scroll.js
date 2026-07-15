53b4771ff3ccd9df613481b4eabb5b06a4ffc6db
Tomas Bozzano <tomasbozzano@arjun.gob.ar>
2026-07-15 15:29:19 -0300

    change view

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -2,6 +2,7 @@ const nav = document.getElementById("header-nav");
 const links = document.querySelectorAll(".nav-link");
 const mobileLinks = document.getElementById("esquina");
 const logoLink = document.getElementById("logo-link");
+const navLinks = document.querySelectorAll(".nav-links a");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
@@ -9,6 +10,34 @@ window.addEventListener("scroll", () => {
     } else {
         nav.classList.remove("scrolled");
     }
+
+    const home = document.querySelector("[data-link-home]");
+    const homeSection = document.getElementById(home.dataset.linkHome);
+    console.log(homeSection);
+
+    navLinks.forEach(link => {
+
+        const id = link.getAttribute("data-link");
+        const section = document.getElementById(id);
+
+        if (!section) return;
+        const top = section.offsetTop;
+        const height = section.offsetHeight;
+        const scroll = window.scrollY + height / 2;
+
+        if (window.scrollY < homeSection.offsetHeight - nav.offsetHeight) {
+            links.forEach(l => l.classList.remove("active"));
+            return;
+        }
+
+        if (scroll >= top - 50 && scroll < top + height - 50) {
+            links.forEach(l => {
+                l.classList.remove("active");
+            });
+            link.classList.add("active");
+        }
+
+    })
 });
 
 
